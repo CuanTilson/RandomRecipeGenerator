1658295e277d24acc00f604733fc0aad731c3f0f
Cúán Tilson <cuantilson@gmail.com>
2025-01-29 12:18:02 +0200

    Add Coil Compose dependency for image loading.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.gson)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
