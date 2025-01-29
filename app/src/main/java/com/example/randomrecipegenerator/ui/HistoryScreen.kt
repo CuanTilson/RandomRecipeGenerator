8dacf4bce5ed730d0f2115f7dee9c8b42c288dab
Cúán Tilson <cuantilson@gmail.com>
2025-01-29 13:08:38 +0200

    Updated the navigation icon in the `HistoryScreen` from `Icons.Filled.ArrowBack` to `Icons.AutoMirrored.Filled.ArrowBack`.

--- app/src/main/java/com/example/randomrecipegenerator/ui/HistoryScreen.kt
+++ app/src/main/java/com/example/randomrecipegenerator/ui/HistoryScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -40,7 +40,7 @@ fun HistoryScreen(viewModel: RecipeViewModel, navController: NavController) {
                 title = { Text(stringResource(R.string.previous_recipes)) },
                 navigationIcon = {
                     IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.Filled.ArrowBack, stringResource(R.string.back))
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, stringResource(R.string.back))
                     }
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
