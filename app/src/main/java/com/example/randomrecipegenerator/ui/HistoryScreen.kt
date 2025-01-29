92acd9ef0ae9ac6dacc3139129435139d780c557
Cúán Tilson <cuantilson@gmail.com>
2025-01-29 13:07:54 +0200

    Added a new `HistoryScreen` that displays a list of previously viewed recipes. Each item in the list, when clicked, navigates the user to the `InfoScreen` of the selected recipe. Added a back button in the `HistoryScreen`'s top bar to allow the user to return to the previous screen. Each item on `HistoryScreen` is `RecipeItem` which display the recipe title and navigates to the recipe's detail page on click. If the recipe title is null it displays a default message.

---
+++ app/src/main/java/com/example/randomrecipegenerator/ui/HistoryScreen.kt
@@ -0,0 +1,90 @@
+package com.example.randomrecipegenerator.ui
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.randomrecipegenerator.R
+import com.example.randomrecipegenerator.model.Recipe
+import com.example.randomrecipegenerator.viewmodel.RecipeViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HistoryScreen(viewModel: RecipeViewModel, navController: NavController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.previous_recipes)) },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, stringResource(R.string.back))
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        val listState = rememberLazyListState()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            state = listState
+        ) {
+            items(viewModel.previousRecipes) { recipe ->
+                RecipeItem(recipe = recipe, viewModel = viewModel, navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun RecipeItem(recipe: Recipe, viewModel: RecipeViewModel, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                // Set the selected recipe in the ViewModel
+                viewModel.setRecipe(recipe)
+                // Navigate to the InfoScreen
+                navController.navigate("info")
+            }
+            .padding(8.dp)
+    ) {
+        Text(
+            text = recipe.title ?: stringResource(R.string.no_title_available),
+            style = MaterialTheme.typography.titleMedium,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+    }
+}
