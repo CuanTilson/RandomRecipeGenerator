f452dda94ca0c7d7b112ded56f9b83b22f8c2d89
Cúán Tilson <cuantilson@gmail.com>
2025-01-29 13:09:11 +0200

    Implemented navigation between recipe, info, loading, error, and history screens using `NavHost` and composable functions. Added `Surface` to the UI for better styling, and moved viewmodel creation to the activity.

--- app/src/main/java/com/example/randomrecipegenerator/ui/MainActivity.kt
+++ app/src/main/java/com/example/randomrecipegenerator/ui/MainActivity.kt
@@ -3,17 +3,47 @@ package com.example.randomrecipegenerator.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.remember
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.randomrecipegenerator.RandomRecipeGeneratorTheme
 import com.example.randomrecipegenerator.viewmodel.RecipeViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: RecipeViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RandomRecipeGeneratorTheme {
-                val viewModel = remember { RecipeViewModel() }
-                AppNavigation(viewModel)
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "recipe") {
+                        composable("recipe") {
+                            RecipeScreen(viewModel, navController)
+                        }
+                        composable("info") {
+                            InfoScreen(viewModel, navController)
+                        }
+                        composable("loading") {
+                            LoadingScreen()
+                        }
+                        composable("error") {
+                            ErrorScreen(message = "Error")
+                        }
+                        composable("history") {
+                            HistoryScreen(viewModel, navController)
+                        }
+                    }
+                }
             }
         }
     }
